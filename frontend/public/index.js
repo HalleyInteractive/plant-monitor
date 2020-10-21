416c839e80b1b5eb8cab14e87f875474b61dbe3c
Niels Overwijn <nielsoverwijn@google.com>
2020-10-21 16:37:06 +0200

    Updated dashboard to include DHT data

--- frontend/public/index.js
+++ frontend/public/index.js
@@ -114,8 +114,8 @@ function updateCharts(snapshot) {
     const log = Object.values(snapshot.val())
     log.forEach(function(d){ 
         d.timestamp = new Date(d.timestamp * 1000) 
-        d.light_perc = ((d.light - lightRange[0]) * 100) / (lightRange[1] - lightRange[0])
-        d.water_perc = ((d.water - waterRange[0]) * 100) / (waterRange[1] - waterRange[0])
+        d.light = ((d.light - lightRange[0]) * 100) / (lightRange[1] - lightRange[0])
+        d.water = ((d.water - waterRange[0]) * 100) / (waterRange[1] - waterRange[0])
     });
     updateLines(log)
     updateDonuts(log[log.length - 1]);
@@ -228,104 +228,15 @@ window.addEventListener('load', function() {
 /**
  * Visualisation 
  */
-const margin = {top: 20, right: 30, bottom: 30, left: 40}
+const margin = {top: 20, right: 30, bottom: 30, left: 40};
 const width = 900 - margin.left - margin.right, height = 400 - margin.top - margin.bottom;
-// append the svg object 
-const svg = d3
-    .select("#graph")
-    .append("svg")
-    .attr('viewBox', `0 0 ${width + margin.left + margin.right} ${height + margin.top + margin.bottom}`)
-
-const graph = svg
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-const xScale = d3.scaleTime().range([0, width])
-const yScaleLight = d3.scaleLinear().range([height, 0])
-const yScaleWater = d3.scaleLinear().range([height, 0])
-
-const xAxisGroup = svg.append("g")
-    .attr("class", "axis")
-    .attr("transform", `translate(50,${height + margin.bottom})`)
-        
-const yAxisLight = svg.append("g")
-    .attr("class", "axis")
-    .attr("transform", `translate(${margin.left}, ${margin.top})`)
-
-const yAxisWater = svg.append("g")
-    .attr("class", "axis")
-    .attr("transform", `translate(${width + margin.left}, ${margin.top})`)
-
-const pathLight = graph
-    .append('g')
-    .append("path")  
-    .attr('fill', 'none')
-    .attr('stroke', '#ef6c00')
-    .attr('stroke-width', 1)
-
-const pathWater = graph
-    .append('g')
-    .append("path")  
-    .attr('fill', 'none')
-    .attr('stroke', '#0288d1')
-    .attr('stroke-width', 1)
-
-const lineLight = d3.line()
-    .defined(d => !isNaN(d.light))
-    .x(d => xScale(d.timestamp))
-    .y(d => yScaleLight(d.light_perc))
-    .curve(d3.curveMonotoneX) 
-
-const lineWater = d3.line()
-    .defined(d => !isNaN(d.water))
-    .x(d => xScale(d.timestamp))
-    .y(d => yScaleWater(d.water_perc))
-    .curve(d3.curveMonotoneX)     
-
-/** 
-* Some simple application logic
-*/
-
-const updateLines = data => {
-    console.log('Updating line data');
-
-    xScale.domain(d3.extent(data, d => d.timestamp))
-    yScaleLight.domain([0, 100])
-    yScaleWater.domain([0, 100])
-
-    xAxisGroup
-        .call(d3.axisBottom(xScale)
-        .tickFormat(d3.timeFormat("%a %H:%M"))
-        .ticks(width / 80).tickSizeOuter(0))
-
-    yAxisLight
-        .call(d3.axisLeft(yScaleLight))
-        .call(g => g.select(".domain").remove())
-        .call(g => g.select(".tick:last-of-type text").clone())
-
-    yAxisWater
-        .call(d3.axisRight(yScaleWater))
-        .call(g => g.select(".domain").remove())
-        .call(g => g.select(".tick:last-of-type text").clone())
-
-    pathLight
-        .datum(data)
-        .attr('d', lineLight)    
-
-    pathWater
-        .datum(data)
-        .attr('d', lineWater)   
-}
-
-
+const lineGraph = initializeLineGraph();
 const donutDims = {width: 180, height: 180, radius: 90, hole: 40}
 const donutColors = ['#64b5f6', '#eee', '#ff9800']
-
 const arc = d3.arc()
     .innerRadius(donutDims.radius - donutDims.hole)
     .outerRadius(donutDims.radius);
-
-let donutgroup = []
+const donutgroup = [];
 document.querySelectorAll('.donut').forEach((element) => {
     donutgroup.push(d3.select(element)
         .append('svg')
@@ -333,62 +244,126 @@ document.querySelectorAll('.donut').forEach((element) => {
         .attr('height', donutDims.height)
         .append('g')
         .attr('class', 'donut-container')
-        .attr('transform', `translate(${ donutDims.width / 2 },${ donutDims.height / 2 })`))   
+        .attr('transform', `translate(${ donutDims.width / 2 },${ donutDims.height / 2 })`));
 });
 
-const updateDonuts = data => {
-    const waterData = [{type: 'water', perc: data.water_perc}, {type: 'rest', perc: 100 - data.water_perc}]
-    const lightData = [{type: 'light', perc: data.light_perc}, {type: 'rest', perc: 100 - data.light_perc}]
-    const colorScale = d3.scaleOrdinal( waterData.map(d => d.type), donutColors )
+function initializeLineGraph() {
+    const lg = {};
+    lg.svg = d3
+        .select("#graph")
+        .append("svg")
+        .attr('viewBox', `0 0 ${width + margin.left + margin.right} ${height + margin.top + margin.bottom}`);
+    lg.graph = lg.svg
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    lg.xScale = d3.scaleTime().range([0, width]);
+    lg.xAxis = lg.svg.append("g")
+        .attr("class", "axis")
+        .attr("transform", `translate(50,${height + margin.bottom})`);
+    lg.water = initialiseLine(lg, 'water', '#0288d1');
+    lg.light = initialiseLine(lg, 'light', '#ef6c00');
+    lg.temperature = initialiseLine(lg, 'temperature', '#e35454');
+    lg.humidity = initialiseLine(lg, 'humidity', '#428af5');
+    return lg;
+}
 
+function initialiseLine(lg, type, color) {
+    const line = {};
+    line.yScale = d3.scaleLinear().range([height, 0]);
+    line.yAxis = lg.svg.append("g")
+        .attr("class", "axis")
+        .attr("transform", `translate(${margin.left}, ${margin.top})`);
+        line.path = lg.graph
+        .append('g')
+        .append("path")  
+        .attr('fill', 'none')
+        .attr('stroke', color)
+        .attr('stroke-width', 1);
+        line.line = d3.line()
+        .defined(d => !isNaN(d[type]))
+        .x(d => lg.xScale(d.timestamp))
+        .y(d => lg[type].yScale(d[type]))
+        .curve(d3.curveMonotoneX);
+    return line;
+}
+
+const updateLines = data => {
+    console.log('Updating line data');
+    lineGraph.xScale.domain(d3.extent(data, d => d.timestamp))
+    lineGraph.light.yScale.domain([0, 100])
+    lineGraph.water.yScale.domain([0, 100])
+    drawLine('light', data);
+    drawLine('water', data);
+    drawLine('temperature', 'temperature' in data[0] ? data : []);
+    drawLine('humidity', 'humidity' in data[0] ? data: []);
+}
+
+function drawLine(line, data) {
+    lineGraph[line].yScale.domain([0, 100]);
+    lineGraph[line].yAxis
+        .call(d3.axisRight(lineGraph[line].yScale))
+        .call(g => g.select(".domain").remove())
+        .call(g => g.select(".tick:last-of-type text").clone());
+    lineGraph[line].path
+    .datum(data)
+    .attr('d', lineGraph[line].line);
+}
+
+function updateDonuts(data) {
+    const waterData = [{type: 'water', perc: data.water}, {type: 'rest', perc: 100 - data.water}];
+    const lightData = [{type: 'light', perc: data.light}, {type: 'rest', perc: 100 - data.light}];
+    const humidityData = [{type: 'humidity', perc: data.humidity}, {type: 'rest', perc: 100 - data.humidity}];
+    const temperatureData = [{type: 'temperature', perc: data.temperature}, {type: 'rest', perc: 100 - data.temperature}];
     const pie = d3.pie()
         .padAngle(0.005)
         .sort(null)
         .value(d => d.perc)
     const pieArcsWater = pie(waterData);
     const pieArcsLight = pie(lightData);
+    const pieArcsHumidity = pie(humidityData);
+    const pieArcsTemperature = pie(temperatureData);
 
-    donutgroup[0].selectAll('path')
-        .data(pieArcsWater)
-        .join('path')
-            .style('stroke', 'white')
-            .style('stroke-width', 2)
-            .style('fill', d => colorScale( d.data.type ))
-            .attr('d', arc)
-
-  	donutgroup[0].selectAll("text")
-  	  .data([waterData])
-  	  .join("text")
-  	  .attr("text-anchor", "middle")
-  	  .attr("dy", ".3em")
-  	  .style("font", "8px sans-serif")
-  	  .style("max-width", "100%")
-  	  .style("height", "auto")
-  	  .attr("text-anchor", "middle")
-  	  .attr("fill", '#616161')
-  	  .text( d => Math.floor(d[0].perc) + "%")
-        .attr("transform", `scale(${ (donutDims.radius - donutDims.hole)/15 })`);    
-        
-
-    donutgroup[1].selectAll('path')
-        .data(pieArcsLight)
-        .join('path')
-            .style('stroke', 'white')
-            .style('stroke-width', 2)
-            .style('fill', d => colorScale( d.data.type ))
-            .attr('d', arc)
-
-  	donutgroup[1].selectAll("text")
-  	  .data([lightData])
-  	  .join("text")
-  	  .attr("text-anchor", "middle")
-  	  .attr("dy", ".3em")
-  	  .style("font", "8px sans-serif")
-  	  .style("max-width", "100%")
-  	  .style("height", "auto")
-  	  .attr("text-anchor", "middle")
-  	  .attr("fill", '#616161')
-  	  .text( d => Math.floor(d[0].perc) + "%")
-  	  .attr("transform", `scale(${ (donutDims.radius - donutDims.hole)/15 })`);           
+    drawDonut(0, pieArcsWater, waterData, '#64b5f6', '%');
+    drawDonut(1, pieArcsLight, lightData, '#ff9800', '%');
+    if('humidity' in data) {
+        drawDonut(2, pieArcsHumidity, humidityData, '#428af5', '%');
+    } else {
+        drawDonut(2, pie([{type:'', perc:''}]), [{type:'', perc:''}], '#fff', '');
+    }
+    if('temperature' in data) {
+        drawDonut(3, pieArcsTemperature, temperatureData, '#e35454', '˚');
+    } else {
+        drawDonut(3, pie([{type:'', perc:''}]), [{type:'', perc:''}], '#fff', '');
+    }
+    
 }
 
+function drawDonut(index, pieArc, data, color, suffix) {
+    const colorScale = d3.scaleOrdinal(data.map(d => d.type), [color, '#eee']);
+    donutgroup[index].selectAll('path')
+        .data(pieArc)
+        .join('path')
+            .style('stroke', 'white')
+            .style('stroke-width', 2)
+            .style('fill', d => colorScale( d.data.type ))
+            .attr('d', arc)
+
+  	donutgroup[index].selectAll("text")
+  	  .data([data])
+  	  .join("text")
+  	  .attr("text-anchor", "middle")
+  	  .attr("dy", ".3em")
+  	  .style("font", "8px sans-serif")
+  	  .style("max-width", "100%")
+  	  .style("height", "auto")
+  	  .attr("text-anchor", "middle")
+  	  .attr("fill", '#616161')
+  	  .text((d) => {
+            if(d[0].perc) {
+                return Math.floor(d[0].perc) + suffix;
+            } else {
+                return '';
+            }
+      })
+  	  .attr("transform", `scale(${ (donutDims.radius - donutDims.hole)/15 })`);
+}
