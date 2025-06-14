93c69f02838aa28339890f7008a6438963c23dad
Niels Overwijn <niels.overwijn@gmail.com>
2025-06-14 17:56:41 +0200

    Update logs template.

--- webapp/src/app/components/logs/logs.ts
+++ webapp/src/app/components/logs/logs.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { EspService } from '../../services/esp-service';
 import { MatListModule } from '@angular/material/list';
+import { JsonPipe } from '@angular/common';
 @Component({
   selector: 'app-logs',
-  imports: [MatListModule],
+  imports: [MatListModule, JsonPipe],
   templateUrl: './logs.html',
   styleUrl: './logs.scss'
 })
