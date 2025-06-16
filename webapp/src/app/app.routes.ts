84a378687e087e757bf176dcc2dc3d13be526e35
Niels Overwijn <niels.overwijn@gmail.com>
2025-06-16 20:28:54 +0200

    Fix nav

--- webapp/src/app/app.routes.ts
+++ webapp/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Flash } from './pages/flash/flash';
+import { Debug } from './pages/debug/debug';
 
 export const routes: Routes = [
   {
@@ -17,5 +18,10 @@ export const routes: Routes = [
     path: 'flash',
     component: Flash,
     title: 'Flash'
+  },
+  {
+    path: 'debug',
+    component: Debug,
+    title: 'Debug'
   }
 ];
--- webapp/src/app/app.ts
+++ webapp/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 import { ConnectionButton } from "./components/connection-button/connection-button";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatToolbarModule, ConnectionButton],
+  imports: [RouterOutlet, MatToolbarModule, ConnectionButton, RouterModule, MatButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
