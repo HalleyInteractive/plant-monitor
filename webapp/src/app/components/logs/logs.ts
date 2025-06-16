fee49fcc74da819067205126c70f1b1999bf1d10
Niels Overwijn <niels.overwijn@gmail.com>
2025-06-16 19:57:57 +0200

    Move logs to card

--- webapp/src/app/components/logs/logs.ts
+++ webapp/src/app/components/logs/logs.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { EspService } from '../../services/esp-service';
 import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-logs',
-  imports: [MatListModule],
+  imports: [MatListModule, MatCardModule],
   templateUrl: './logs.html',
   styleUrl: './logs.scss'
 })
