e399a2b42944b2035ae347eb1714555abca311c7
Niels Overwijn <niels.overwijn@gmail.com>
2025-06-17 21:54:43 +0200

    Changed to material

--- webapp/src/app/pages/debug/debug.ts
+++ webapp/src/app/pages/debug/debug.ts
@@ -2,11 +2,25 @@ import { Component, inject } from '@angular/core';
 import { EspService } from '../../services/esp-service';
 import { CommonModule } from '@angular/common'; // For *ngIf, *ngFor, etc.
 import { FormsModule } from '@angular/forms'; // For ngModel if needed later, or template-driven forms
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
 
 @Component({
   selector: 'app-debug',
-  imports: [CommonModule, FormsModule], // Add CommonModule and FormsModule
-  standalone: true, // Assuming this is a standalone component
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatTooltipModule,
+  ],
+  standalone: true,
   templateUrl: './debug.html',
   styleUrl: './debug.scss'
 })
